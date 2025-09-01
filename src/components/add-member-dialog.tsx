96b604c67ea25e440ea34e70979a84d95acc98ab
StrenX Fit <strenxfit@gmail.com>
2025-09-01 18:56:38 +0000

    when pick a date ,calender month name goes out of the screen

--- src/components/add-member-dialog.tsx
+++ src/components/add-member-dialog.tsx
@@ -220,7 +220,7 @@ export default function AddMemberDialog() {
                             </Button>
                           </FormControl>
                         </PopoverTrigger>
-                        <PopoverContent className="w-auto p-0" align="start">
+                        <PopoverContent className="p-0" align="start">
                           <Calendar mode="single" selected={field.value} onSelect={field.onChange} disabled={(date) => date > new Date() || date < new Date("1900-01-01")} initialFocus />
                         </PopoverContent>
                       </Popover><FormMessage />
