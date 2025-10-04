2bb776e1322d2ec64263b090992d4f69d2e5a3f0
StrenX Fit <strenxfit@gmail.com>
2025-10-04 16:16:25 +0000

    Try fixing this error: `Runtime Error: ReferenceError: cn is not defined

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
+
 import type {Metadata} from 'next';
 import { Inter } from 'next/font/google';
 import Script from 'next/script';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from '@/components/theme-provider';
+import { cn } from '@/lib/utils';
 
 const inter = Inter({ subsets: ['latin'], variable: '--font-inter' });
 
