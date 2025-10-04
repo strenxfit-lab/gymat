239248a4efa162d0581ff2491d725547cb3b68f2
StrenX Fit <strenxfit@gmail.com>
2025-10-04 13:10:57 +0000

    Try fixing this error: `Runtime Error: Error: cn is not defined. Error s

--- src/app/dashboard/owner/community/page.tsx
+++ src/app/dashboard/owner/community/page.tsx
@@ -18,6 +18,7 @@ import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as z from 'zod';
 import Image from 'next/image';
+import { cn } from "@/lib/utils";
 
 
 interface Post {
