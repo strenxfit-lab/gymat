d7d681171a74d54a137609989040a319a4321144
strenxfit-lab <strenxfit@gmail.com>
2025-10-08 22:38:53 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,7 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -22,6 +24,7 @@ const nextConfig = {
       },
     ],
   },
+  transpilePackages: ['recharts'],
 };
 
-module.exports = nextConfig;
+export default nextConfig;
