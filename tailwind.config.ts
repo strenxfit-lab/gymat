5b72ae07d45cb463f30213e32de6f77abec77e32
strenxfit-lab <strenxfit@gmail.com>
2025-10-08 20:47:42 +0530

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,12 +14,6 @@ export default {
         headline: ['var(--font-inter)', 'sans-serif'],
         code: ['monospace'],
       },
-      borderRadius: {
-        lg: 'var(--radius)',
-        md: 'calc(var(--radius) - 2px)',
-        sm: 'calc(var(--radius) - 4px)',
-        '2xl': 'calc(var(--radius) + 4px)',
-      },
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
@@ -71,30 +65,11 @@ export default {
           border: 'hsl(var(--sidebar-border))',
           ring: 'hsl(var(--sidebar-ring))',
         },
-        indigo: {
-          '500': '#6366F1',
-        },
-        purple: {
-            '500': '#8B5CF6',
-            '400': '#A78BFA',
-        },
-        emerald: {
-          '500': '#10B981',
-        },
-        gray: {
-          '100': '#F3F4F6',
-          '300': '#D1D5DB',
-          '500': '#6B7280',
-          '700': '#374151',
-          '900': '#111827',
-        },
-        green: {
-          '500': '#22C55E',
-        },
-        red: {
-          '500': '#EF4444',
-        },
-        'accent-red': '#FF4B4B',
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
       },
       keyframes: {
         'accordion-down': {
